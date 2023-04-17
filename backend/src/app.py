c6599ca75f1aa42c2d090bd2306e81f42f1e5c68
WalterLuong <wluong@student.42.fr>
2023-04-17 16:56:11 +0200

    minor updates

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,41 +4,48 @@ from dotenv import load_dotenv
 from flask import Flask, request
 from auth import auth
 
-# CREATE_ROOMS_TABLE = (
-#     "CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT);"
-# )
+CREATE_ROOMS_TABLE = (
+    "CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT);"
+)
 
-# CREATE_TEMPS_TABLE = """CREATE TABLE IF NOT EXISTS temperatures (room_id INTEGER, temperature REAL,
-#                         date TIMESTAMP, FOREIGN KEY(room_id) REFERENCES rooms(id) ON DELETE CASCADE);"""
+CREATE_TEMPS_TABLE = """CREATE TABLE IF NOT EXISTS temperatures (room_id INTEGER, temperature REAL,
+                        date TIMESTAMP, FOREIGN KEY(room_id) REFERENCES rooms(id) ON DELETE CASCADE);"""
 
-# INSERT_ROOM_RETURN_ID = "INSERT INTO rooms (name) VALUES (%s) RETURNING id;"
+INSERT_ROOM_RETURN_ID = "INSERT INTO rooms (name) VALUES (%s) RETURNING id;"
 
-# INSERT_TEMP = (
-#     "INSERT INTO temperatures (room_id, temperature, date) VALUES (%s, %s, %s);"
-# )
+INSERT_TEMP = (
+    "INSERT INTO temperatures (room_id, temperature, date) VALUES (%s, %s, %s);"
+)
 
-# GLOBAL_NUMBER_OF_DAYS = (
-#     """SELECT COUNT(DISTINCT DATE(date)) AS days FROM temperatures;"""
-# )
+GLOBAL_NUMBER_OF_DAYS = (
+    """SELECT COUNT(DISTINCT DATE(date)) AS days FROM temperatures;"""
+)
 
-# GLOBAL_AVG = """SELECT AVG(temperatures) as average FROM temperatures;"""
+GLOBAL_AVG = """SELECT AVG(temperatures) as average FROM temperatures;"""
 
 load_dotenv()
 
 app = Flask(__name__)
-# url = os.getenv("DATABASE_URL")
-# connection = psycopg2.connect(url)
+url = os.getenv("DATABASE_URL")
 
-# @app.post("/api/room")
-# def create_room():
-#     data = request.get_json()
-#     name = data["name"]
-#     with connection:
-#         with connection.cursor() as cursor:
-#             cursor.execute(CREATE_ROOMS_TABLE)
-#             cursor.execute(INSERT_ROOM_RETURN_ID, (name,))
-#             room_id = cursor.fetchone()[0]
-#     return {"id": room_id, "message":f'Room {name} created'}, 201
+try:
+    connection = psycopg2.connect(url)
+except:
+    print("\033[1;31mDatabase is not ready...\033[m")
+
+connection = psycopg2.connect(url)
+print("\033[1;32mDatabase is connected !\033[m")
+
+@app.post("/api/room")
+def create_room():
+    data = request.get_json()
+    name = data["name"]
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_ROOMS_TABLE)
+            cursor.execute(INSERT_ROOM_RETURN_ID, (name,))
+            room_id = cursor.fetchone()[0]
+    return {"id": room_id, "message":f'Room {name} created'}, 201
 
 @app.get("/")
 def main_page():
